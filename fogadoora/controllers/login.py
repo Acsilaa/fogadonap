1d4f9733c5f6cd47f2b049c0541298b5ca500e39
Gergo Szentjobi <gszentjobi@gmail.com>
2023-11-09 11:26:07 +0100

    started login page

---
+++ fogadoora/controllers/login.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def validateLogin(username, password):
+        if (isValidPass(password) and isVaslidUname(username)):
+                pass # TODO 
+         
