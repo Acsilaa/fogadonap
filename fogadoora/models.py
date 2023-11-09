b341684cf949566b65dd00e3c0dca762fc2a4140
Gál Attila Péter <gal.attila.peter@burg.lo.hu>
2023-11-09 11:24:33 +0100

    after merge, db almost done

--- fogadoora/models.py
+++ fogadoora/models.py
@@ -1,32 +1,50 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
+
+class defuser(models.Model):
+    username = models.CharField(max_length=40)
+    password = models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
+    group = models.IntegerField()
+
+class ClassificationAsText(models.Model):
+    classification = models.CharField(max_length=20)
 
 class Teacher(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    first = models.IntegerField()
-    last = models.IntegerField()
+    userID = models.IntegerField()
 
-# class Person():
-#     name = models.CharField(max_length=50)
+class TeacheableSubjects(models.Model):
+    teacher = models.IntegerField()
+    english = models.BooleanField(default=0)
+    german = models.BooleanField(default=0)
+    literature = models.BooleanField(default=0)
+    mathematics = models.BooleanField(default=0)
+    history = models.BooleanField(default=0)
+    arts = models.BooleanField(default=0)
 
-# class Parent(Person, models.Model):
-#     child = models.CharField() #mert több gyermeke is lehet
+class Student(models.Model):
+    clazz = models.CharField(max_length=5)
+    parent = models.IntegerField()
 
-# class Student(Person, models.Model):
-#     clazz = models.CharField(max_length=4)
-#     parent = models.IntegerField()
+class Parent(models.Model):
+    userID = models.IntegerField()
+    children = models.CharField(max_length=20) #str=array, separated by ','
 
+class Formteacher(models.Model):
+    userID = models.IntegerField()
+    clazz = models.CharField(max_length=5)
 
-# class Formteacher(Person, models.Model):
-#     clazz = models.CharField(max_length=4)
+class TeachersOfClass(models.Model):
+    teacher = models.IntegerField()
+    clazz = models.CharField(max_length=5)
 
-# class Classes(models.Model):
-#     pass
+class Appointments(models.Model):
+    teacher = models.IntegerField()
+    _from = models.IntegerField()
+    to = models.IntegerField()
+    parent = models.IntegerField()
 
-# class Appointments(models.Model):
-#     pass
-
-# class TeachingInClass(models.Model):
-#     pass
+class Allowance(models.Model):
+    teacher = models.IntegerField()
+    isAllowed = models.BooleanField(default=1)
 
