8afd76b23219713a1880e2212e921ec6cb6040a1
al144 <davidasztral2007@gmail.com>
2023-11-09 13:10:55 +0100

    username validation

--- fogadoora/controllers/validation.py
+++ fogadoora/controllers/validation.py
@@ -1,34 +1,52 @@
+from .. import models as m 
 
+def isValidPass(pwd): 
+    pwd = list(pwd)
 
-def isValidPass(pass): 
-        pass = list(pass)
+    def passHasInt(pwd):
 
-    def hasInt(pass):
+         for i in pwd:
+             if i.isDigit():
+                 return True
 
-        for i in pass:
-            if i.isDigit():
-                return True
-
-    def hasChar(pass):
+    def passHasChar(pwd):
         
         chars = ["#", "!", "@", "(", ")", "*", "%"]
-        for i in pass:
-            if i in chars
-            return True
+        for i in pwd:
+            if i in chars:
+                return True
 
-    def hasLoverCase(pass):
+    def passHasLover(pwd):
 
-        for i in pass:
+        for i in pwd:
             if i.islover():
                 return True
     
-    def hasUpper(pass):
+    def passHasUpper(pwd):
 
-        for i in pass:
+        for i in pwd:
             if i.isupper():
                 return True
 
-    if hasUpper() and hasLover() and hasChar() and hasInt():
+    def passIsLong(pwd):
+        
+        if len(pwd) <= 7:
+            return False
+
+    if passHasUpper() and passHasLover() and passHasChar() and passHasInt() and passIsLong():
         return True
     else:
         return False
+
+def isValidUname(uname):
+
+    def unIsLong(uname):
+        if (m.doesUserExist()):
+            if len(uname) > 0 and len(uname) < 40:
+                return True
+            else:
+                return False
+    
+    return unIsLong()
+   
+        
